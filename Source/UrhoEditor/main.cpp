a05485f4da3a00ef23277af1759414235d466327
yululiao <1059770532@qq.com>
2023-11-12 22:39:19 +0800

    MSVC define WinMain

--- Source/UrhoEditor/main.cpp
+++ Source/UrhoEditor/main.cpp
@@ -1,9 +1,16 @@
 #include "EditorApp.h"
 
 using namespace Urho3DEditor;
-
-int main(int argc, char* argv[])
-{ 
-	EditorApp::getInstance()->run(); 
+#ifdef _MSC_VER
+int WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+{
+	EditorApp::getInstance()->run();
 	return 0;
 }
+#else
+int main(int argc, char* argv[])
+{
+	EditorApp::getInstance()->run();
+	return 0;
+}
+#endif
