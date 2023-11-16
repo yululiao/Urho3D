8d1afe0f3acf02f561193818a03dcc4b3a49824e
yulu.lyl <yulu.lyl@alibaba-inc.com>
2023-11-16 20:39:13 +0800

    Editor Console

--- Source/UrhoEditor/main.cpp
+++ Source/UrhoEditor/main.cpp
@@ -4,9 +4,15 @@ using namespace Urho3DEditor;
 #ifdef _MSC_VER
 int WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
 {
+	//打开控制台
+    AllocConsole();
+    SetConsoleOutputCP(CP_UTF8);
+    freopen("CONOUT$", "w", stdout);
+	//----------
 	EditorApp::getInstance()->run();
 	return 0;
 }
+
 #else
 int main(int argc, char* argv[])
 {
